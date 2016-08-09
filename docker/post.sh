8b1be5611aaff0a7c0a7d32536444dbe0e0900f3
Carleton Coffrin <carleton@coffrin.com>
2016-08-09 15:30:59 -0600

    adding script for posting grader to the platform

---
+++ docker/post.sh
@@ -0,0 +1,57 @@
+# Course id :THkzKaBPEeWm9xJA1YXcWQ
+# Item ids:
+#          - FktbJ
+# knapsack - TvXJx
+# coloring - npnKe
+# tsp      - sUgcy
+# facility - GywH2
+# vrp      - ewEUu
+# setcover - 0tD43
+
+courseraprogramming upload coursera_do_grader THkzKaBPEeWm9xJA1YXcWQ FktbJ mJWtY \
+  --additional_item_and_part TvXJx awPVV \
+  --additional_item_and_part TvXJx hHYWS \
+  --additional_item_and_part TvXJx JwWnx \
+  --additional_item_and_part TvXJx Z2tMt \
+  --additional_item_and_part TvXJx PUIxa \
+  --additional_item_and_part TvXJx AKXWc \
+  --additional_item_and_part npnKe fmYLC \
+  --additional_item_and_part npnKe IkKpq \
+  --additional_item_and_part npnKe pZOjO \
+  --additional_item_and_part npnKe XDQ31 \
+  --additional_item_and_part npnKe w7hAO \
+  --additional_item_and_part npnKe tthbm \
+  --additional_item_and_part sUgcy YQL5Q \
+  --additional_item_and_part sUgcy R9hfg \
+  --additional_item_and_part sUgcy ZVrLp \
+  --additional_item_and_part sUgcy 6tyFn \
+  --additional_item_and_part sUgcy tSpoY \
+  --additional_item_and_part sUgcy YOVlV \
+  --additional_item_and_part GywH2 BsWZ1 \
+  --additional_item_and_part GywH2 rOIP7 \
+  --additional_item_and_part GywH2 Umtuf \
+  --additional_item_and_part GywH2 gkoSJ \
+  --additional_item_and_part GywH2 1T4Zv \
+  --additional_item_and_part GywH2 YjE7f \
+  --additional_item_and_part GywH2 3hnIJ \
+  --additional_item_and_part GywH2 bFCUi \
+  --additional_item_and_part ewEUu Q7wCC \
+  --additional_item_and_part ewEUu 5c314 \
+  --additional_item_and_part ewEUu nUAU4 \
+  --additional_item_and_part ewEUu QQs4z \
+  --additional_item_and_part ewEUu EV1zA \
+  --additional_item_and_part ewEUu 5U5So \
+  --additional_item_and_part 0tD43 KMnkb \
+  --additional_item_and_part 0tD43 eMZfU \
+  --additional_item_and_part 0tD43 VrksQ \
+  --additional_item_and_part 0tD43 Xfvao \
+  --additional_item_and_part 0tD43 9xtmX \
+  --additional_item_and_part 0tD43 GghJ7 
+
+sleep 600
+
+courseraprogramming publish THkzKaBPEeWm9xJA1YXcWQ FktbJ \
+  --additional-items TvXJx npnKe sUgcy GywH2 ewEUu 0tD43
+
+
+
